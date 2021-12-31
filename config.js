fee19e13b26aca1f4f78af98df0d8c8f8befb95e
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2021-12-31 10:53:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['6281333154367', '6287755080455'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = 'Rizal'
+global.author = 'Gans'
 
 global.wm = '© ariffb & melcanz'
 global.wait = '_*tunggu sedang diproses...*_'
